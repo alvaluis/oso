43b1ca2982033128470f15e89b9662b99ff92453
Alvaro <alvaroapril@hotmail.com>
2014-11-03 17:36:39 +0100

    Implementar inicializar jugadores.

--- jugadores_vacio.c
+++ jugadores_vacio.c
@@ -12,7 +12,24 @@
  * E inicializa el turno al primer jugador.
  */
 void inicializar_jugadores(t_jugadores *js) {
-  /* COMPLETAR */
+	int i;	
+	printf("Numero total de jugadores (2-6): ");
+	scanf("%d", &(*js).num_jugadores);	
+	if((*js).num_jugadores >= MIN_JUGADORES && (*js).num_jugadores <= MAX_JUGADORES)
+	{
+		printf("Numero de jugadores humanos (0-%d): ", (*js).num_jugadores);  
+		scanf("%d",&(*js).num_humanos);
+		if((*js).num_humanos >= MIN_HUMANOS && (*js).num_humanos <= (*js).num_jugadores)
+		{
+			(*js).turno = 0;		
+			for(i = 0; i < (*js).num_jugadores; i++)			
+				(*js).j[i].num_osos = 0; 	
+			for (i = 0; i < (*js).num_humanos; i++)
+				(*js).j[i].tipo = JUGADOR_HUMANO;
+			for(i = (*js).num_humanos; i < (*js).num_jugadores; i++)
+				(*js).j[i].tipo = JUGADOR_MAQUINA;
+		}
+	}
 }
 
 /*
