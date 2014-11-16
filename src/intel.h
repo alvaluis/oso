7f0a5f6237d7807d4e501806cb0728308e42b7da
Xavier Mendez <me@jmendeth.com>
2014-11-16 13:08:48 +0100

    INTEL: Documentar las funciones

--- src/intel.h
+++ src/intel.h
@@ -15,9 +15,36 @@ typedef struct {
     int num;
 } t_lista_jugadas;
 
-void determinar_jugada(t_mapa *mapa, t_jugadores *jugadores, t_jugada *jugada);
+/*
+ * void jugadas_validas(t_mapa *mapa, t_lista_jugadas *dst);
+ * Dado un mapa, llena dst con todas las jugadas legales que se
+ * podrían efectuar en dicho mapa.
+ */
 void jugadas_validas(t_mapa *mapa, t_lista_jugadas *dst);
+
+/*
+ * void determinar_jugada(t_mapa *mapa, t_jugadores *jugadores, t_jugada *jugada);
+ * Método principal de la IA. Dado mapa y jugadores, escoge
+ * una jugada a realizar por la máquina.
+ *
+ * Precondición: existe por lo menos una jugada válida.
+ */
+void determinar_jugada(t_mapa *mapa, t_jugadores *jugadores, t_jugada *jugada);
+
+/*
+ * void filtrar_maximo_score(t_mapa *mapa, t_jugadores *jugadores, t_lista_jugadas *src, t_lista_jugadas *dst);
+ * Calcula la puntuación (score) de cada jugada en src
+ * y deja la(s) jugadas con mayor score en dst.
+ *
+ * Precondición: existe por lo menos una jugada en src.
+ */
 void filtrar_maximo_score(t_mapa *mapa, t_jugadores *jugadores, t_lista_jugadas *src, t_lista_jugadas *dst);
+
+/*
+ * float calcular_score(t_mapa *mapa, t_jugadores *jugadores, t_jugada *jugada);
+ * Dado mapa y jugadores, calcula la puntuación (score) que tendría la
+ * jugada si se hiciera por el jugador que tiene el turno.
+ */
 float calcular_score(t_mapa *mapa, t_jugadores *jugadores, t_jugada *jugada);
 
 #endif /* INTEL_H */
