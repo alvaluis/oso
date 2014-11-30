ba01774e7cb636a263af81e6360d350f7f85ddc3
Xavier Mendez <me@jmendeth.com>
2014-11-30 22:53:39 +0100

    Permitir usar las flechas para escoger casilla

--- README.md
+++ README.md
@@ -20,9 +20,14 @@ Una vez hecho esto, se puede ejecutar el juego con `./juego`.
 
 Lista de mejoras respecto del juego original:
 
+ - En el momento de introducir fila y columna, pueden usarse las flechas para escoger la casilla.
+   Una vez hecho esto, presionar immediatamente la letra que se desea poner y se hace la jugada.
  - La entrada de datos no tolera letras ni entra en bucle infinito.
  - La entrada de datos ignora (descarta) parametros sobrantes al final de la línea.
  - Prescindir de la constante `NINGUN_JUGADOR`.
  - Mejoras en Inteligencia Artificial.
  - La inteligencia artificial acepta puntero a `t_jugadores`, para tener más información del contexto.
  - El azar se inicializa de forma más precisa, para que varie con más frecuencia.
+
+La intención es que estas mejoras pasen desapercibidas, de manera que a primera vista el juego se comporta como el original.
+La mayoría de ellas son a nivel de estructura del código, pero tambien hay algunas que añaden características al juego.
--- src/juego.c
+++ src/juego.c
@@ -7,36 +7,192 @@
 #include "intel.h"
 #include "input.h"
 
+#define ESTADO_INTRODUCIR  1
+#define ESTADO_SELECCIONAR 2
+#define CAMPO_POSICION 1
+#define CAMPO_LETRA    2
+#define CAMPO_NINGUNO  3
+#define MAXIMO_ENTRADA 10
+
+/*
+ * En el momento de pedir columna, imprime la posición (f,c)
+ * como si la hubiera escrito el usuario y situa el cursor
+ * en la posición indicada del mapa impreso anteriormente.
+ */
+static void situar_cursor(t_mapa *mapa, int f, int c) {
+    /* Ir a la posición guardada y limpiar lo que hay "introducido" */
+    printf("\e[u\e[K");
+    /* Imprimir la posición */
+    printf("%2d, %2d", f, c);
+    /* Situarse en la casilla de la esquina inferior izquierda */
+    printf("\e[3F");
+    /* Subir hasta la fila correspondiente */
+    if (f < mapa->num_filas-1)
+        printf("\e[%dF", (mapa->num_filas-1) - f);
+    /* Avanzar hasta la columna correspondiente */
+    printf("\e[%dG", 4 + c * 2);
+}
+
 /*
  * Dado un mapa, un numero de jugador;
- * pide fila, columna y un caracter 'O' o 'S' a un jugador humano,
- * verifica fila,col,car y realiza la jugada.
+ * Pide fila, columna y un carácter 'O' o 'S' a un jugador humano,
+ * verifica que sean válidos y realiza la jugada.
  * Devuelve el numero de OSOs conseguidos con esa jugada.
+ *
+ * ## Método de entrada
+ *
+ * Hay dos estados, "introducir" y "seleccionar".
+ *
+ *  - Introducir. El usuario debe teclear las coordenadas de la
+ *    casilla, pulsar Enter, introducir la letra, y pulsar Enter.
+ *    Este es el modo inicial.
+ *
+ *  - Seleccionar. El usuario usa las flechas del teclado para
+ *    mover el cursor de la pantalla hasta situar este sobre
+ *    la casilla que quiera. Con el cursor encima de una casilla
+ *    vacía, presiona la letra que desea poner.
+ *
+ * Si el usuario está en modo introducir, al presionar cualquier
+ * flecha se borrará lo que haya puesto y se pasará al modo
+ * seleccionar, con el cursor en la casilla del medio del tablero.
+ *
+ * Si el usuario está en modo seleccionar, al presionar Suprimir
+ * o Backspace se le devolverá al modo Introducir de nuevo.
  */
 int jugar_humano(t_mapa *mapa, int j) {
     int f, c, r;
-    char car;
+    char car, i;
 
-    while (1) {
-        printf(" Fila,Columna: ");
-        r = scanf("%d,%d", &f,&c);
-        descartar_linea();
+    int valido;
+    int estado = ESTADO_INTRODUCIR;
+    int campo = CAMPO_POSICION;
+    char entrada [MAXIMO_ENTRADA+1] = "";
+    int entrada_l = 0;
 
-        if ((r == 2)&&
-            (f >= 0 && f < mapa->num_filas)&&
-            (c >= 0 && c < mapa->num_cols)&&
-            (mapa->c[f][c].letra == CASILLA_VACIA))
-            break;
+    adquirir_tty();
+    printf(" Fila,Columna: \e[s");
+
+    while (campo != CAMPO_NINGUNO) {
+        i = fgetc(stdin);
+        if (estado == ESTADO_INTRODUCIR) {
+            if (i == '\n') {
+
+                /* ESTADO:   INTRODUCIR *
+                 * CARÁCTER: NEWLINE    */
+                entrada[entrada_l] = '\0';
+                if (campo == CAMPO_POSICION) {
+                    r = sscanf(entrada, "%d,%d", &f, &c);
+                    valido = (r == 2)
+                             && (f >= 0 && f < mapa->num_filas)
+                             && (c >= 0 && c < mapa->num_cols)
+                             && (mapa->c[f][c].letra == CASILLA_VACIA);
+                } else {
+                    r = sscanf(entrada, "%c", &car);
+                    car = toupper(car);
+                    valido = (r == 1) && (car == 'S' || car == 'O');
+                }
+
+                if (valido) {
+                    entrada_l = 0;
+                    printf("\r\n");
+                    campo++;
+                    if (campo == CAMPO_LETRA)
+                        printf("Letra [O/S]: ");
+                } else {
+                    while (entrada_l > 0) {
+                        printf("\e[D");
+                        entrada_l--;
+                    }
+                    printf("\e[K");
+                }
+
+            } else if (i == '\e') {
+
+                /* ESTADO:   INTRODUCIR *
+                 * CARÁCTER: ESCAPE     */
+                if (fgetc(stdin) == '[') {
+                    i = fgetc(stdin);
+                    if (i >= 'A' && i <= 'D') {
+                        f = mapa->num_filas / 2;
+                        c = mapa->num_cols / 2;
+                        situar_cursor(mapa, f, c);
+                        estado = ESTADO_SELECCIONAR;
+                    }
+                }
+
+            } else if (i == 0x7F || i == '\b') {
+
+                /* ESTADO:   INTRODUCIR      *
+                 * CARÁCTER: DEL / BACKSPACE */
+                if (entrada_l > 0) {
+                    entrada_l--;
+                    printf("\e[D\e[K");
+                }
+
+            } else if (isprint(i)) {
+
+                /* ESTADO:   INTRODUCIR *
+                 * CARÁCTER: IMPRIMIBLE */
+                if (entrada_l < MAXIMO_ENTRADA) {
+                    entrada[entrada_l++] = i;
+                    printf("%c", i);
+                }
+
+            }
+        } else {
+            if (isspace(i)) {
+
+                /* ESTADO:   SELECCIONAR *
+                 * CARÁCTER: ESPACIADO   */
+                if (mapa->c[f][c].letra == CASILLA_VACIA) {
+                    printf("\e[u\e[K%2d, %2d\r\nLetra [O/S]: ", f, c);
+                    entrada_l = 0;
+                    estado = ESTADO_INTRODUCIR;
+                    campo = CAMPO_LETRA;
+                }
+
+            } else if (i == '\e') {
+
+                /* ESTADO:   SELECCIONAR *
+                 * CARÁCTER: ESCAPE      */
+                if (fgetc(stdin) == '[') {
+                    i = fgetc(stdin);
+                    if (i == 'A') {
+                        if (f > 0) situar_cursor(mapa, --f, c);
+                    } else if (i == 'D') {
+                        if (c > 0) situar_cursor(mapa, f, --c);
+                    } else if (i == 'B') {
+                        if (f < mapa->num_filas-1) situar_cursor(mapa, ++f, c);
+                    } else if (i == 'C') {
+                        if (c < mapa->num_cols-1)  situar_cursor(mapa, f, ++c);
+                    }
+                }
+
+            } else if (i == 0x7F || i == '\b') {
+
+                /* ESTADO:   SELECCIONAR     *
+                 * CARÁCTER: DEL / BACKSPACE */
+                printf("\e[u\e[K");
+                entrada_l = 0;
+                estado = ESTADO_INTRODUCIR;
+                campo = CAMPO_POSICION;
+
+            } else if (isprint(i)) {
+
+                /* ESTADO:   SELECCIONAR *
+                 * CARÁCTER: IMPRIMIBLE  */
+                i = toupper(i);
+                if (mapa->c[f][c].letra == CASILLA_VACIA && (i == 'S' || i == 'O')) {
+                    car = i;
+                    printf("\e[u\e[K%2d, %2d\r\nLetra [O/S]: %c\r\n", f, c, car);
+                    campo = CAMPO_NINGUNO;
+                }
+
+            }
+        }
     }
 
-    while (1) {
-        printf("Letra [O/S]: ");
-        scanf("%c", &car);
-        descartar_linea();
-
-        car = toupper(car);
-        if (car == 'O' || car == 'S') break;
-    }
+    restablecer_tty();
 
     escribir_jugada(mapa, j, f, c, car);
     return calcular_osos(mapa, f, c, car);
@@ -126,6 +282,8 @@ int main() {
     t_mapa mapa;
 
     inicializar_azar();
+    inicializar_tty();
+
     inicializar_jugadores(&js);
     inicializar_mapa(&mapa);
 
