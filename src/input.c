0e006169c9a9271b8351b6839cca39ed4e6ddbab
Xavier Mendez <me@jmendeth.com>
2014-11-30 22:53:13 +0100

    Implementar funciones para TTY en input

--- src/input.c
+++ src/input.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <termios.h>
 #include "input.h"
 
 void descartar_linea() {
@@ -20,3 +25,30 @@ int pedir_entero(const char *prefijo, const char *nombre, int min, int max) {
     }
 }
 
+static struct termios inicial, actual;
+
+static void signal_handler(int sig) {
+    exit(1);
+}
+
+void inicializar_tty() {
+    /* Por ahora no estamos interesados en comprobar
+     * que los cambios en la TTY se efectuen. */
+    tcgetattr(fileno(stdin), &inicial);
+    memcpy(&actual, &inicial, sizeof(struct termios));
+
+    atexit(restablecer_tty);
+    signal(SIGTERM, signal_handler);
+}
+
+void adquirir_tty() {
+    memcpy(&actual, &inicial, sizeof(struct termios));
+    actual.c_lflag &= ~ICANON;
+    actual.c_lflag &= ~ECHO;
+    tcsetattr(fileno(stdin), TCSANOW, &actual);
+}
+
+void restablecer_tty() {
+    memcpy(&actual, &inicial, sizeof(struct termios));
+    tcsetattr(fileno(stdin), TCSANOW, &actual);
+}
--- src/input.h
+++ src/input.h
@@ -15,5 +15,28 @@ void descartar_linea();
  */
 int pedir_entero(const char *prefijo, const char *nombre, int min, int max);
 
+/*
+ * void inicializar_tty();
+ * Obtiene información del estado actual del pseudo-terminal,
+ * necesaria para el correcto funcionamiento de las demás funciones.
+ * Se asegura que el pseudo-terminal se restablece al salir.
+ * Debe llamarse una única vez.
+ */
+void inicializar_tty();
+
+/*
+ * void adquirir_tty();
+ * Ajusta el pseudo-terminal para aplicaciones interactivas;
+ * los caracteres se pasan immediatamente al programa y no
+ * se ven en el terminal.
+ */
+void adquirir_tty();
+
+/*
+ * void restablecer_tty();
+ * Restablece el terminal a su estado inicial.
+ */
+void restablecer_tty();
+
 #endif /* INPUT_H */
 
